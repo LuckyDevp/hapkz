8daa2d08a63cc081fd6937c7ee4618a670c0607c
Lucky's <sourcdevs@outlook.com>
2025-05-11 17:24:31 -0500

    Delete src directory

--- src/cpp-compiler.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-
-function compileCPP(inputFile, outputFile) {
-  execSync(`g++ ${inputFile} -o ${outputFile}`, { stdio: 'inherit' });
-  console.log(`[SUCCESS] ${outputFile} generated!`);
-}
-
-module.exports = { compileCPP };
--- src/js-compiler.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-
-function compileJS(inputFile, outputFile) {
-  execSync(`pkg ${inputFile} --target=win --output ${outputFile}`, { stdio: 'inherit' });
-  console.log(`[SUCCESS] ${outputFile} generated!`);
-}
-
-module.exports = { compileJS };
